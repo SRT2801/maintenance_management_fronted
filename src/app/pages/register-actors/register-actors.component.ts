18feeaf1012bca42996c12a3e99f92de471038d8
sahir ruiz <sahirruiz4@gmail.com>
2024-10-27 12:09:38 -0500

    "Updated Register Actors component to use FormGroup and FormControl, added form validation, and modified HTML template to include form tag and input component"

--- src/app/pages/register-actors/register-actors.component.ts
+++ src/app/pages/register-actors/register-actors.component.ts
@@ -1,50 +1,85 @@
 import { Component } from '@angular/core';
-import { IRole } from '../../interfaces/IRole';
+import { IRole, Role } from '../../interfaces/IRole';
 import { HttpService } from '../../shared/services/http/http.service';
 import { IActor } from '../../interfaces/IActor';
+import { DocumentType } from '../../enums/actor/DocumentType';
+import { Status } from '../../enums/actor/Status';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 interface Document {
   name: string;
 }
 
-interface Role {
-  role: string;
-}
+
 interface State {
   status: string;
 }
 @Component({
   selector: 'app-register-actors',
   templateUrl: './register-actors.component.html',
-  styleUrl: './register-actors.component.css'
+  styleUrl: './register-actors.component.css',
 })
 export class RegisterActorsComponent {
   constructor(private readonly _httpSrv: HttpService) {}
   public roles!: IRole;
   public actor!: IActor;
+  public registerForm!: FormGroup;
 
+  get nameControl() {
+    return this.registerForm.get('name') as FormControl;
+  }
+
+  get lastNameControl() {
+    return this.registerForm.get('last_name') as FormControl;
+  }
+
+  get phoneControl() {
+    return this.registerForm.get('phone_number') as FormControl;
+  }
+
+  get emailControl() {
+    return this.registerForm.get('email') as FormControl;
+  }
+
+  get documentNumberControl() {
+    return this.registerForm.get('document_number') as FormControl;
+  }
   value!: string;
-  name!: string;
-  lastName!: string;
-  phone!: number;
-  email!: string;
-  document!: number;
-  documentType!: string;
 
   documents!: Document[];
   selectedDocuments!: Document[];
   selectedRoles!: Role[];
 
-  status!: State[]
+  status!: State[];
   selectedStatus!: State[];
 
   ngOnInit() {
-    this.getRoles()
+    this.getRoles();
+
+    this.registerForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      last_name: new FormControl('', Validators.required),
+      phone_number: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\d{10}$/)
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      document_number: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\d+$/)
+      ]),
+      document_type: new FormControl('', Validators.required),
+      status: new FormControl('', Validators.required),
+      role: new FormControl('', Validators.required),
+    });
 
     this.documents = [
-      { name: 'Cédula de ciudadanía' },
-      { name: 'Cédula de extranjería' },
-      { name: 'Pasaporte' },
-      { name: 'Registro civil' },
+      { name: DocumentType.CC },
+      { name: DocumentType.CE },
+      { name: DocumentType.PB },
+      { name: DocumentType.RC },
     ];
 
     /* this.roles = [
@@ -53,9 +88,9 @@ export class RegisterActorsComponent {
     ]; */
 
     this.status = [
-      { status: 'Active' },
-      { status: 'Inactive' },
-      { status: 'Suspended' },
+      { status: Status.ACTIVE },
+      { status: Status.INACTIVE },
+      { status: Status.SUSPENDED },
     ];
   }
 
@@ -63,19 +98,47 @@ export class RegisterActorsComponent {
     this._httpSrv.get<IRole>('roles/').subscribe({
       next: (data) => {
         this.roles = data;
-        const roles = this.roles.data;
-        console.log('🚀 ~ HomeComponent ~ getRoles ~ roles:', roles);
+        this.selectedRoles = this.roles.data;
+        console.log('Roles cargados:', this.selectedRoles);
       },
       error(err) {
-        console.error(err);
+        console.error('Error al obtener roles:', err);
       },
     });
   }
 
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const formValue = this.registerForm.value;
+      console.log('Formulario válido:', formValue);
+
+      const actor: IActor = {
+        name: formValue.name,
+        last_name: formValue.last_name,
+        phone_number: formValue.phone_number,
+        email: formValue.email,
+        document_number: formValue.document_number,
+        document_type: formValue.document_type.name,
+        status: formValue.status.status,
+        role: formValue.role.id
+      };
+
+      this.saveActors(actor);
+    } else {
+      console.error('El formulario es inválido:', this.registerForm.errors);
+    }
+  }
+
   //Debes pasarle el body, imagino será con lo del form.value quizá, ese sería el body (el json, con el cuerpo del actor, debe ser el de la interfaz)
   protected async saveActors(body: IActor) {
-    this._httpSrv.post<IActor>('actors/', body).subscribe(res => {
-      console.log(res);
-    })
-  }
+  this._httpSrv.post<IActor>('actors/', body).subscribe({
+    next: (res) => {
+      console.log('Actor registrado exitosamente:', res);
+    },
+    error: (err) => {
+      console.error('Error al registrar actor:', err.error);
+    },
+  });
+}
 }
---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,17 @@
+// input.component.ts
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css',
+})
+export class InputComponent {
+  @Input() control!: FormControl;
+  @Input() label!: string;
+
+  public keyUp(event: any) {
+    this.control.setValue(event.target.value);
+  }
+}
