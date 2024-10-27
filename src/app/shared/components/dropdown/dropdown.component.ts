47c0377c360148579feb3f83f1ac44569dc91a9c
sahir ruiz <sahirruiz4@gmail.com>
2024-10-27 12:11:14 -0500

    Fix: implements metod accessors in this component

--- src/app/shared/components/dropdown/dropdown.component.ts
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,13 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrl: './dropdown.component.css'
+  styleUrls: ['./dropdown.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DropdownComponent),
+      multi: true,
+    },
+  ],
 })
-export class DropdownComponent {
+export class DropdownComponent implements ControlValueAccessor {
   @Input() options!: any[];
-  @Input() selectedOption!: any;
   @Input() optionLabel!: string;
   @Input() placeholder!: string;
+
+  value!: any;
+  isDisabled: boolean = false;
+
+  onChange = (value: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+
+  onSelectChange(event: any) {
+    this.value = event.value;
+    this.onChange(this.value);  
+  }
 }
