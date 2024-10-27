de4b3849cb0088a39798466f5e19d1c9e9cd71d1
sahir ruiz <sahirruiz4@gmail.com>
2024-10-27 12:10:21 -0500

    Feat: was added button component reusable

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() text: string = 'Click me';
+  @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>();
+
+  onClick() {
+    this.buttonClick.emit();
+  }
+}
