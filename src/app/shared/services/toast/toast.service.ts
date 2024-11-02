e2d5a575c8111615abd0cc9ea4de869a7afe3dc3
sahir ruiz <sahirruiz4@gmail.com>
2024-11-02 18:03:39 -0500

    Feat: was added toast service

---
+++ src/app/shared/services/toast/toast.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  showToast(message: string, action: string = 'Close', duration: number = 3000) {
+    this.snackBar.open(message, action, {
+      duration: duration,
+      verticalPosition: 'top',
+      horizontalPosition: 'center'
+    });
+  }
+}
